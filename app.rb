0e54c808c228c548b549cf06a2b6d41fa4ea210c
Philipp Obrivanov <milliardex@gmail.com>
2021-08-04 18:00:52 +0300

    Add menu with products

--- app.rb
+++ app.rb
@@ -9,10 +9,14 @@ set :database, { adapter: "sqlite3", database: "pizzashop.db" }
 class Product < ActiveRecord::Base
 end
 
+before do
+  @products = Product.all
+end
+
 get '/' do
   erb :index 
 end
 
-get '/about' do
-  erb :about
+get '/menu' do
+  erb :menu
 end
