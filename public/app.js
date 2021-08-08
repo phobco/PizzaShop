0e03a7994985f14c0f34c0b8e9e5162fd761156e
Philipp Obrivanov <milliardex@gmail.com>
2021-08-08 18:00:47 +0300

    Add add_to_cart function stub

--- public/app.js
+++ public/app.js
@@ -2,10 +2,15 @@ function something()
 {
 	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-	x = x * 1 + 1;
+	x = x * 1 + 1; // x = x + 1
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 	alert(x);
 
+}
+
+function add_to_cart()
+{
+	alert('Hello from function!');
 }
