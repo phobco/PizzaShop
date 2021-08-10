d82000e183801617db711f370d4f73fb907fbf4c
Philipp Obrivanov <milliardex@gmail.com>
2021-08-10 23:43:58 +0300

    Add buttons and functions for products

--- public/app.js
+++ public/app.js
@@ -10,11 +10,53 @@ function something()
 
 }
 
+
 function add_to_cart(id)
 {
-	var key = 'product_'+ id;
+	var key = 'product_' + id;
 
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+}
+
+
+
+
+	function show_cart()
+	{
+		
+		var result = 0;
+
+		for (var i = 0; i < localStorage.length; i++)
+		{
+
+		    var x = window.localStorage.getItem(localStorage.key(i));
+
+			result = result * 1 + x * 1;
+		}
+			
+		alert(result);
+		
+	}
+
+function info(id)
+{
+	var key = 'product_' + id;
+	var x = window.localStorage.getItem(key);
+
+	if (x == null) 
+	{
+		x = 0;
+	}
+
+	alert(x + ' in cart')
+}
+
+function delete_product(id)
+{
+	var key = 'product_' + id;
+	var value = window.localStorage.getItem(key);
+	value = value * 1 - 1;
+	window.localStorage.setItem(key, value);
 }
