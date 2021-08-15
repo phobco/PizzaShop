6a40e97b0f3aeb0706a4c1d13dcf54c75691f83f
Philipp Obrivanov <milliardex@gmail.com>
2021-08-15 14:05:24 +0300

    Add new products in db

--- db/migrate/20210804094608_add_products.rb
+++ db/migrate/20210804094608_add_products.rb
@@ -30,6 +30,56 @@ class AddProducts < ActiveRecord::Migration[6.1]
        :is_veg => true,
        :is_best_offer => false,
        :path_to_image => '/images/veg.jpg'
-   })  
+   })
+   
+   Product.create ({:title => 'Neapolitan',
+    :description => 'Neapolitan is a traditional pizza',
+    :price => 370,
+    :size => 30,
+    :is_spicy => false,
+    :is_veg => true,
+    :is_best_offer => false,
+    :path_to_image => '/images/neapolitan.jpg'
+    })
+
+    Product.create ({:title => 'New York-Style Pizza',
+      :description => 'Boasting a thin, foldable base with cheese',
+      :price => 300,
+      :size => 30,
+      :is_spicy => false,
+      :is_veg => true,
+      :is_best_offer => false,
+      :path_to_image => '/images/ny_style.jpg'
+   })
+
+   Product.create ({:title => 'Chicago/Deep-Dish Pizza',
+    :description => 'This style of pizza is banked in a pan',
+    :price => 390,
+    :size => 30,
+    :is_spicy => false,
+    :is_veg => true,
+    :is_best_offer => false,
+    :path_to_image => '/images/chicago.jpg'
+   })
+   
+   Product.create ({:title => 'Sicillian',
+    :description => 'Hot Sicillian pizza',
+    :price => 420,
+    :size => 30,
+    :is_spicy => false,
+    :is_veg => true,
+    :is_best_offer => false,
+    :path_to_image => '/images/sicillian.jpg'
+   })
+
+   Product.create ({:title => 'Californian',
+    :description => 'Very tasty Californian pizza',
+    :price => 320,
+    :size => 30,
+    :is_spicy => false,
+    :is_veg => true,
+    :is_best_offer => false,
+    :path_to_image => '/images/california.jpg'
+   })
   end
 end
