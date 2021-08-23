26f5f757fed94d6708f28b76a7b46a8ad8e41a7f
Philipp Obrivanov <milliardex@gmail.com>
2021-08-23 15:16:34 +0300

    Add parse_orders_input method

--- app.rb
+++ app.rb
@@ -18,6 +18,7 @@ end
 
 before do
   @products = Product.all
+  @c = Order.new
 end
 
 get '/' do
@@ -28,12 +29,19 @@ get '/menu' do
   erb :menu
 end
 
-get '/cart' do
-  @c = Order.new
+post '/cart' do
+  orders_input = params[:orders]
+  @items = parse_orders_input (orders_input)
+  
+  @items.each do |item|
+    # item = [id, cnt]
+    item[0] = @products.find(item[0])
+  end
+
   erb :cart
 end
-
-post '/order' do
+  
+post '/order' do 
   @c = Order.new params[:order]
   if @c.save
     erb :order
@@ -41,4 +49,26 @@ post '/order' do
     @error = @c.errors.full_messages.first
     erb :cart
   end
+end
+
+def parse_orders_input (orders_input)
+  s1 = orders_input.split(",")
+  
+  arr = []
+  
+  s1.each do |item|
+    s2 = item.split("=")
+    
+    s3 = s2[0].split("_")
+
+    id = s3[1]
+    cnt = s2[1]
+    
+    arr_tmp = [id, cnt]
+    
+    if arr_tmp[1] != '0'
+      arr.push arr_tmp
+    end
+  end
+    return arr
 end
